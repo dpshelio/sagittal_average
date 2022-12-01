nuttamas/rsd-sagital_average

Adds a setup file to enable installation of the package

setup.py
@@ -1,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sagital_average",
+    version="0.1.0",
+    packages=find_packages(exclude=['*test']),
+)

dpshelio/sagittal_average

setup.py
@@ -1,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sagital_average",
+    version="0.1.0",
+    packages=find_packages(exclude=['*tests']),
+)