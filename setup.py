6e4a883fdea3804fb5db098f3263e98b68beaa09
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2022-12-01 10:25:11 +0000

    Adds `setup.cfg` to set package installation parameters
    It simplifies the `setup.py` maintenance

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="sagittal_average",
-    version="0.1.0",
-    packages=find_packages(exclude=['*tests']),
-    install_requires=['numpy'],
-    entry_points={
-        'console_scripts': [
-            'sagverage = sagittal_average.command:process'
-            ]},
-)
+setup()
